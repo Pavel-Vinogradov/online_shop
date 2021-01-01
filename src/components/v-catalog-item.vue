554e1f5e1efe41bae4843fa704ebabf1221e7d6f
Pavel <65782910+Pavel-Vinogradov@users.noreply.github.com>
2021-01-01 14:55:45 +0300

    add props

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,17 +1,25 @@
 <template>
-<div class="v-catalog-item">
-  <img :src="require('../assets/img/'+item.image)"
-       :alt="item.image">
-  <p> {{item.article}}</p>
-  <p> Цена {{item.price}} р</p>
-  <button class="btn">Add to cart</button>
-</div>
+  <div class="v-catalog-item">
+    <img :src="require('../assets/img/'+item.image)"
+         :alt="item.image">
+    <p> {{ item.name }}</p>
+    <p> Цена {{ item.price }} р</p>
+    <button @click="sendDataToParent"
+            class="v-catalog-item__add_to_cart_btn btn">Add to cart
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-name: "v-catalog-item",
-  props:{item:Object},
+  name: "v-catalog-item",
+  props: {item: Object},
+  computed: {},
+  methods: {
+    sendDataToParent() {
+      this.$emit('sendDataToParent', this.item.article)
+    }
+  }
 }
 </script>
 
--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -1,68 +1,77 @@
 <template>
-<div class="v-catalog__list">
-<v-catalog-item
- v-for="item in products" v-bind:key="item.article" v-bind:item="item"/>
-</div>
+  <div class="v-catalog__list">
+    <v-catalog-item
+        @sendDataToParent="showArticle"
+        v-for="item in products"
+        v-bind:key="item.article"
+        v-bind:item="item"/>
+  </div>
 </template>
 
 <script>
 import VCatalogItem from "@/components/v-catalog-item";
+
 export default {
-name: "v-catalog",
+  name: "v-catalog",
   components: {VCatalogItem},
-  data(){
-  return{
-    products: [
-      {
-        "image": "1.jpg",
-        "name": "T-shirt 1",
-        "price": 2100.234234234,
-        "article": "T1",
-        "available": true,
-        "category": "Мужские"
-      },
-      {
-        "image": "2.jpg",
-        "name": "T-shirt 2",
-        "price": 3150.12312412,
-        "article": "T2",
-        "available": true,
-        "category": "Женские"
-      },
-      {
-        "image": "3.jpg",
-        "name": "T-shirt 3",
-        "price": 4200.51524,
-        "article": "T3",
-        "available": false,
-        "category": "Женские"
-      },
-      {
-        "image": "4.jpg",
-        "name": "T-shirt 4",
-        "price": 5300.1245512,
-        "article": "T4",
-        "available": true,
-        "category": "Мужские"
-      },
-      {
-        "image": "5.jpg",
-        "name": "T-shirt 5",
-        "price": 6500.3522314,
-        "article": "T5",
-        "available": false,
-        "category": "Женские"
-      },
-      {
-        "image": "6.jpeg",
-        "name": "T-shirt 6",
-        "price": 8700.4124123,
-        "article": "T6",
-        "available": true,
-        "category": "Женские"
-      }
-    ]
-  }
+  data() {
+    return {
+      products: [
+        {
+          "image": "1.jpg",
+          "name": "T-shirt 1",
+          "price": 2100.234234234,
+          "article": "T1",
+          "available": true,
+          "category": "Мужские"
+        },
+        {
+          "image": "2.jpg",
+          "name": "T-shirt 2",
+          "price": 3150.12312412,
+          "article": "T2",
+          "available": true,
+          "category": "Женские"
+        },
+        {
+          "image": "3.jpg",
+          "name": "T-shirt 3",
+          "price": 4200.51524,
+          "article": "T3",
+          "available": false,
+          "category": "Женские"
+        },
+        {
+          "image": "4.jpg",
+          "name": "T-shirt 4",
+          "price": 5300.1245512,
+          "article": "T4",
+          "available": true,
+          "category": "Мужские"
+        },
+        {
+          "image": "5.jpg",
+          "name": "T-shirt 5",
+          "price": 6500.3522314,
+          "article": "T5",
+          "available": false,
+          "category": "Женские"
+        },
+        {
+          "image": "6.jpeg",
+          "name": "T-shirt 6",
+          "price": 8700.4124123,
+          "article": "T6",
+          "available": true,
+          "category": "Женские"
+        }
+      ]
+    }
+  },
+  methods: {
+    showArticle(data) {
+      console.log(data);
+    }
   }
 }
 </script>
